047e049e5eb5a37fb5bffe776c4199d9016a091d
Tigris <Tigris>
2016-12-13 15:36:33 +0530

    Sp Comments

--- module/mysqli.js
+++ module/mysqli.js
@@ -19,11 +19,11 @@ var mysqli = [];
 
 mysqli['smslog'] = "call addSMSlog(?, ?, ?, ?, ?)";
 mysqli['getdatasp'] = "call getServiceStation( ? )";
-mysqli['getreminders'] = "call getReminders( ?, ? )";
-mysqli['updatereminders'] = "call updateReminders( ? )";
-mysqli['statistics'] = "call getStatistics(?)";
-mysqli['gettemplates'] = "call smsTemplates(?)";
-mysqli['smslogwithnocount'] = "call smsLogwithnoCount(?, ?, ?, ?, ?)";
-mysqli['certificatereminder'] = "call certificateReminder( )";
-mysqli['cronLog'] = "call getcronLog( ?, ?)";
+mysqli['getreminders'] = "call getReminders( ?, ? )"; // Interval_day: Integer :  Difference from current date; lastday: Integer : Select data from Lastday or Not values 1 or 0; Used to handle Sunday
+mysqli['updatereminders'] = "call updateReminders( ? )"; // reminder_id : Integer
+mysqli['statistics'] = "call getStatistics(?)"; // type: Weekly/daily
+mysqli['gettemplates'] = "call smsTemplates(?)"; // get template event from smstemplates
+mysqli['smslogwithnocount'] = "call smsLogwithnoCount(?, ?, ?, ?, ?)"; // smsdata: SMS test; counts: SMS message Count; ssid : ServiceStation id; _tonumber : Sent Phone number; _customerid: Customer ID
+mysqli['certificatereminder'] = "call certificateReminder( )"; 
+mysqli['cronLog'] = "call getcronLog( ?, ?)";  // _cron: values are either insert/getlog ; _type: Type of reminder event 
 
